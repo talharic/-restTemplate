2468bb3afeee17c44e6c56487d1238fcccfadd6d
Talha Arıç <talharic@gmail.com>
2022-03-02 01:52:56 +0300

    Added getPostBySearch and getPostById

--- src/main/java/com/talharic/resttemplate/controller/RestTemplateController.java
+++ src/main/java/com/talharic/resttemplate/controller/RestTemplateController.java
@@ -1,8 +1,10 @@
 package com.talharic.resttemplate.controller;
 
+import com.talharic.resttemplate.dto.PostDto;
 import com.talharic.resttemplate.service.RestTemplateService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,19 +21,24 @@ public class RestTemplateController {
     }
 
     @GetMapping
-    public ResponseEntity<List<?>> getAllPosts() {
+    public ResponseEntity<List<PostDto>> getAllPosts() {
         return ResponseEntity.ok(restTemplateService.getAllPosts());
     }
 
-    /*
-    @GetMapping("/{id}")
-    public ResponseEntity<PostDto> getPostById(@PathVariable Long id) {
-        return ResponseEntity.ok(restTemplateService.getPostById(id));
-    }
+
     @GetMapping("/{search}")
     public ResponseEntity<List<PostDto>> getPostBySearch(@PathVariable String search) {
         return ResponseEntity.ok(restTemplateService.getPostBySearch(search));
     }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable Long id) {
+        return ResponseEntity.ok(restTemplateService.getPostById(id));
+    }
+
+
+/*
     @PostMapping
     public ResponseEntity<List<PostDto>> createPost(@Valid @RequestBody PostDto post) {
         return ResponseEntity.ok(restTemplateService.create(post));
--- src/main/java/com/talharic/resttemplate/service/RestTemplateService.java
+++ src/main/java/com/talharic/resttemplate/service/RestTemplateService.java
@@ -1,15 +1,16 @@
 package com.talharic.resttemplate.service;
 
+import com.talharic.resttemplate.dto.PostDto;
 import com.talharic.resttemplate.dto.converter.PostDtoConverter;
 import com.talharic.resttemplate.model.Post;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -27,15 +28,36 @@ public class RestTemplateService {
         this.postDtoConverter = postDtoConverter;
     }
 
-    public List<?> getAllPosts() {
-        ResponseEntity<List<Post>> result = restTemplate.exchange(
-                apiBaseUrl + "/posts",
-                HttpMethod.GET, null, new ParameterizedTypeReference<List<Post>>() {
-                });
-
-        return result
-                .getBody()
+    public List<PostDto> getAllPosts() {
+        return getPostList()
                 .stream()
                 .map(postDtoConverter::convert).collect(Collectors.toList());
     }
+
+    public List<PostDto> getPostBySearch(String search) {
+        return getPostList()
+                .stream()
+                .map(postDtoConverter::convert)
+                .filter(x -> x.getBody().toString().contains(search))
+                .collect(Collectors.toList());
+    }
+
+
+    public PostDto getPostById(Long id) throws PostNotFoundException {
+        PostDto postDto = postDtoConverter.convert(Objects.requireNonNull(restTemplate.getForObject(
+                apiBaseUrl + "/posts/" + id,
+                Post.class)));
+        return postDto;
+    }
+
+
+
+
+    protected List<Post> getPostList() {
+        return restTemplate.exchange(
+                apiBaseUrl + "/posts",
+                HttpMethod.GET, null, new ParameterizedTypeReference<List<Post>>() {
+                }).getBody();
+    }
+
 }
